f7ac55e8a61b47aa07d48bd8d130fec5abe36bfe
MyeongQ <gmk0904@ajou.ac.kr>
2023-02-12 22:00:40 +0900

    AllRecipe.com 칵테일 프로필 크롤링 완료

--- Codes/WebCrawler/allrecipe_crawler.py
+++ Codes/WebCrawler/allrecipe_crawler.py
@@ -7,10 +7,14 @@ from selenium.common import exceptions
 import requests
 import os
 import pickle
+import json
 
 IMPLICIT_WAIT = 8
+COLLECT_PROFILES = 1
+COLLECT_REVIEWS = 2
 file_name = 'All_Recipes_Links.txt'
 file_path = os.path.join(os.path.curdir, file_name)
+need_crawl = False
 
 recipe_links = list()
 item_id = 1
@@ -49,8 +53,10 @@ driver = create_driver()
 
 # 레시피 링크 리스트 파일이 없으면 링크 탐색
 if os.path.isfile(file_path):
-    with open(file_path, 'w+') as file:
-        recipe_links = pickle.load(file)
+    with open(file_path, 'r', encoding='utf8') as file:
+        for line in file:
+            recipe_links.append(line[:-1])
+    print(recipe_links)
 
 else:
     # navigate to a website
@@ -73,21 +79,21 @@ else:
 
     # 중복 제거
     recipe_links = list(set(recipe_links))
-    with open(file_path, 'w+') as line:
-        pickle.dump(recipe_links, line)
+    with open(file_path, 'w', encoding='utf8') as fline:
+        for link in recipe_links:
+            fline.write(link + '\n')
 
 # test용
 cnt = 0
 
-driver.close()
 
 # 레시피 링크별 데이터 스크래핑
 for recipe in recipe_links:
-    if cnt == 5: break
-    cnt += 1
+    #if cnt == 5: break
+    #cnt += 1
 
     driver.get(recipe)
-    driver.implicitly_wait(IMPLICIT_WAIT)
+    driver.implicitly_wait(IMPLICIT_WAIT) # 컨텐츠 로딩까지 잠시 대기
 
     container = driver.find_element(By.TAG_NAME, 'article')
     item_profile = dict()
@@ -105,33 +111,36 @@ for recipe in recipe_links:
     finally:
         item_profile['AverageRating'] = rating_avg
 
-    content = container.find_element(By.ID, 'loc.article-content')
+    content = container.find_element(By.CLASS_NAME, 'loc.article-content')
     content_data = content.find_element(By.ID, 'article-content_1-0')
-    recipe_details = content.find_element(By.ID, 'recipe_details_1-0')
+    recipe_details = content_data.find_element(By.ID, 'recipe-details_1-0')
     recipe_details_items = recipe_details.find_elements(By.CLASS_NAME, 'mntl-recipe-details__item')
 
     for item in recipe_details_items:
-        recipe_details_label = item.find_element(By.CLASS_NAME, 'mntl-recipe-details__lable').text[:-1]
+        recipe_details_label = item.find_element(By.CLASS_NAME, 'mntl-recipe-details__label').text[:-1]
         recipe_details_value = item.find_element(By.CLASS_NAME, 'mntl-recipe-details__value').text
         item_profile[recipe_details_label] = recipe_details_value
 
-    ingredients_list = content_data.find_element(By.ID, 'mntl-structured-ingredients__list')
+    ingredients_list = content_data.find_element(By.CLASS_NAME, 'mntl-structured-ingredients__list')
     ingredients_items = ingredients_list.find_elements(By.CLASS_NAME, 'mntl-structured-ingredients__list-item')
     ingredients = []
+
     for item in ingredients_items:
         ingredient = item.find_elements(By.TAG_NAME, 'span')
-        ingredient_quantity = ingredient[0].text
-        ingredient_unit = ingredient[1].text
+        ingredient_quantity = ingredient[0].text + ' '
+        ingredient_unit = ingredient[1].text + ' '
         ingredient_name = ingredient[2].text
-        ingredients.append({"name": ingredient_name,
-                            "quantity": ingredient_quantity,
-                            "unit": ingredient_unit})
+        #ingredients.append({"name": ingredient_name,
+        #                    "quantity": ingredient_quantity,
+        #                    "unit": ingredient_unit})
+        ingredients.append(ingredient_quantity + ingredient_name + ingredient_unit)
+    item_profile["Ingredients"] = ingredients
 
+    directions_list = content_data.find_element(By.ID, 'mntl-sc-block_2-0')
+    directions_steps = directions_list.find_elements(By.TAG_NAME, 'p')
+    directions = [step.text for step in directions_steps]
 
-    #article_body = header.find_element()
-
-
-
+    item_profile["Directions"] = directions
 
     item_list.append(item_profile)
     item_id += 1
@@ -140,12 +149,18 @@ for recipe in recipe_links:
 page_source = driver.page_source
 
 # close the browser
-#driver.close()
+driver.close()
+
+output_json = {"cocktail_profiles": item_list}
+with open("all_recipe_cocktail_profiles.json", "w") as outfile:
+    json.dump(output_json, outfile)
 
 print('category')
+"""
 print(len(category_links))
 for l in category_links:
     print(l)
+"""
 
 print('recipe')
 print(len(recipe_links))
